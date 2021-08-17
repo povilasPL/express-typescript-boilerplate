ada77bda369e5bf70c3715d2680caa0782462328
Povilas Plauska <>
2021-08-17 23:20:25 +0300

    changed imports naming according to convention

--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -2,9 +2,9 @@ import express from "express";
 import { body } from "express-validator";
 
 import { CommonRoutesConfig } from "../common/common.routes.config";
-import UsersController from "./controllers/users.controller";
-import UsersMiddleware from "./middleware/users.middleware";
 import BodyValidationMiddleware from "../common/middleware/body.validation.middleware";
+import usersController from "./controllers/users.controller";
+import usersMiddleware from "./middleware/users.middleware";
 import jwtMiddleware from "../auth/middleware/jwt.middleware";
 import commonPermissionMiddleware from "../common/middleware/common.permission.middleware";
 import { PermissionLevel } from "../common/middleware/common.permissionlevel.enum";
@@ -22,7 +22,7 @@ export class UsersRoutes extends CommonRoutesConfig {
         commonPermissionMiddleware.minimumPermissionLevelRequired(
           PermissionLevel.ADMIN_PERMISSION
         ),
-        UsersController.listUsers
+        usersController.listUsers
       )
       .post(
         body("email").isEmail(),
@@ -30,21 +30,21 @@ export class UsersRoutes extends CommonRoutesConfig {
           .isLength({ min: 6 })
           .withMessage("Password should be at least 6 characters"),
         BodyValidationMiddleware.verifyBodyFieldsErrors,
-        UsersMiddleware.validateSameEmailDoesntExist,
-        UsersController.createUser
+        usersMiddleware.validateSameEmailDoesntExist,
+        usersController.createUser
       );
 
-    this.app.param(`userId`, UsersMiddleware.extractUserId);
+    this.app.param(`userId`, usersMiddleware.extractUserId);
 
     this.app
       .route(`/users/:userId`)
       .all(
-        UsersMiddleware.validateUserExists,
+        usersMiddleware.validateUserExists,
         jwtMiddleware.validJWTNeeded,
         commonPermissionMiddleware.onlySameUserOrAdminCanDoThisAction
       )
-      .get(UsersController.getUserById)
-      .delete(UsersController.removeUser);
+      .get(usersController.getUserById)
+      .delete(usersController.removeUser);
 
     this.app.put(`/users/:userId`, [
       body("email").isEmail(),
@@ -55,12 +55,12 @@ export class UsersRoutes extends CommonRoutesConfig {
       body("lastName").isString(),
       body("permissionLevel").isInt(),
       BodyValidationMiddleware.verifyBodyFieldsErrors,
-      UsersMiddleware.validateSameEmailBelongToSameUser,
-      UsersMiddleware.userCantChangePermissionLevel,
+      usersMiddleware.validateSameEmailBelongToSameUser,
+      usersMiddleware.userCantChangePermissionLevel,
       commonPermissionMiddleware.minimumPermissionLevelRequired(
         PermissionLevel.PAID_PERMISSION
       ),
-      UsersController.put,
+      usersController.put,
     ]);
 
     this.app.patch(`/users/:userId`, [
@@ -73,12 +73,12 @@ export class UsersRoutes extends CommonRoutesConfig {
       body("lastName").isString().optional(),
       body("permissionLevel").isInt().optional(),
       BodyValidationMiddleware.verifyBodyFieldsErrors,
-      UsersMiddleware.validatePatchEmail,
-      UsersMiddleware.userCantChangePermissionLevel,
+      usersMiddleware.validatePatchEmail,
+      usersMiddleware.userCantChangePermissionLevel,
       commonPermissionMiddleware.minimumPermissionLevelRequired(
         PermissionLevel.PAID_PERMISSION
       ),
-      UsersController.patch,
+      usersController.patch,
     ]);
 
     this.app.put(`/users/:userId/permissionLevel/:permissionLevel`, [
@@ -87,7 +87,7 @@ export class UsersRoutes extends CommonRoutesConfig {
       commonPermissionMiddleware.minimumPermissionLevelRequired(
           PermissionLevel.FREE_PERMISSION
       ),
-      UsersController.updatePermissionLevel,
+      usersController.updatePermissionLevel,
   ]);
 
     return this.app;
